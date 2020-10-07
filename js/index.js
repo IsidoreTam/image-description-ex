02d5ff5eca4d7fe467644f49641047221b6fac1a
IsidoreTam <59669931+IsidoreTam@users.noreply.github.com>
2020-10-07 14:09:46 -0600

    commit

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
 window.addEventListener('load', function(e){
-    var featureLink = document.querySelector('citation')
+    var featureLink = document.querySelector('citation');
+
+    function featureLinkHandler() {
+        let featureImage = document.querySelector('img.feature');
+        featureImage.src = featureLink.href;
+        featureImage.classList.remove('hidden');
+       }
+
+    featureLink.addEventListener('click', featureLinkHandler);
 })
